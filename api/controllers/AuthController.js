ffd5a89cda97d562a69b170afeaa0060bcf5950f
Michael Wanyoike <micwanyoike@gmail.com>
2018-01-22 20:10:29 +0300

    Implemented auth form

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,14 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing Auths
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	authenticate: async (request, response) => {
+
+	}
+
+};
