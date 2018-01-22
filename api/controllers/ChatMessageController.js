b5918574fa0540c40749a87dd716be5cd1ad73f2
Michael Wanyoike <micwanyoike@gmail.com>
2018-01-22 12:53:53 +0300

    Generated Chat Message API

---
+++ api/controllers/ChatMessageController.js
@@ -0,0 +1,11 @@
+/**
+ * ChatMessageController
+ *
+ * @description :: Server-side logic for managing Chatmessages
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
